7827dc37ebc3fb9567d7a391da45883b0dd6bc05
0384359 <0384359@schoolsnet.act.edu.au>
2022-02-22 15:13:44 +1100

    made gun work

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,15 +1,23 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(int) var countdownMax
+var currentTimer
 
-
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene treenfor the first time.
 func _ready():
-	pass # Replace with function body.
+	currentTimer = countdownMax
+	$HUD/Countdown.text = str(currentTimer)
+
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("GAME OVER")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
-var movement_speed = 750
-# Declare member variables here. Examples:
+var movement_speed = 200
+var bulletSource = preload ("res://Bullet/Bullet.tscn")
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_process(true)
 	set_physics_process(true)
-
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
@@ -18,3 +18,8 @@ func _physics_process(delta):
 	
 #func _process(delta):
 #	pass
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
