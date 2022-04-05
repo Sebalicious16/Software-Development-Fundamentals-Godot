0eba6f4abec0f94a837adb23b18d5cd9fd63c74e
0384359 <0384359@schoolsnet.act.edu.au>
2022-04-05 15:11:03 +1000

    better title screen

--- menu/new_script.gd
+++ menu/new_script.gd
@@ -9,7 +9,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for button in $"Layout/Main/Buttons/GameScenes".get_children():
-	button.connect("pressed", self, "_on Button_Pressed", [button.scene_to_load])
+		button.connect("pressed", self, "_on Button_Pressed", [button.scene_to_load])
 
 func _on_Button_Pressed(scene_to_load):
 	print(scene_to_load)
