032f3783947ff9b9ffa8ada0516359c5f41fa528
0384359 <0384359@schoolsnet.act.edu.au>
2022-04-28 11:58:50 +1000

    bouncing enemies

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
-#var speed = 200
+func _ready():
+	$Area2D.connect("area_entered", self, "_colliding")
+	
 
-#func _ready():
-	#set_physics_process(true)
-
-#func _physics_process(delta):
-	#move_and_collide(Vector2(speed * delta, 0))
+func _colliding(area):
+	print("here")
+	if area.is_in_group("right"):
+		get_parent().global_position.y += 10
+		get_parent().speed = get_parent().speed * -1
